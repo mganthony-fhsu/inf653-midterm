d3e7831fd2b9aed10c03a01416f7c2cbfffe8810
mganthony-fhsu <mganthony@mail.fhsu.edu>
2022-03-13 18:06:39 -0400

    configure db changes for heroku

--- config/Database.php
+++ config/Database.php
@@ -1,19 +1,26 @@
 <?php 
 class Database {
     // DB params
-    private $url = getenv('JAWSDB_URL');
-    private $dbparts = parse_url($this->url);
-
-    private $host = $this->dbparts['host'];
-    private $db_name = ltrim($this->dbparts['path'],'/');
-    private $username = $this->dbparts['user'];
-    private $password = $this->dbparts['pass'];
-
+    private $url;
+    private $dbparts;
+    
+    private $host;
+    private $username;
+    private $password;
+    private $db_name;
 
     private $conn;
 
     // DB connect
     public function connect() {
+        $this->url = getenv('JAWSDB_URL');
+        $this->dbparts = parse_url($this->url);
+    
+        $this->host = $this->dbparts['host'];
+        $this->username = $this->dbparts['user'];
+        $this->password = $this->dbparts['pass'];
+        $this->db_name = ltrim($this->dbparts['path'],'/');
+
         $this->conn = null;
         try {
             $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name,
